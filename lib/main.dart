07df543858c4d9f7fbcdac3f8b9626331e55c754
Francisco Cucaita <pacho.cucaita@gmail.com>
2021-10-22 20:01:26 -0300

    authors

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         shadowColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
         title: Text(
           "Home",
           style: TextStyle(
@@ -23,22 +26,53 @@ class MyApp extends StatelessWidget {
       ),
       body: SingleChildScrollView(
           child: Padding(
-        padding: const EdgeInsets.all(12.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(children: <Widget>[
-          TextField(
-            onChanged: (value) {
-              print(value);
-            },
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Title, authors, or topics'),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.find_in_page, color: Colors.black),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  hintText: 'Title, authors, or topics'),
+              onChanged: (value) {},
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Categories",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.blue,
+                        height: 10,
+                        width: 10,
+                      )
+                    ],
+                  ),
+                  Container(
+                    height: 200,
+                    color: Colors.red,
+                  ),
+                ],
+              )),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
                   "Trending Books",
                   style: const TextStyle(
                     color: Colors.black,
@@ -47,24 +81,22 @@ class MyApp extends StatelessWidget {
                   ),
                   textDirection: TextDirection.ltr,
                 ),
-              ),
-              Text(
-                "See All",
-                style: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+                Text(
+                  "See All",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.grey,
+                  ),
                 ),
-                textDirection: TextDirection.ltr,
-              ),
-            ],
+              ],
+            ),
           ),
           Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Column(
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
                     children: <Widget>[
                       Image.asset(
                         "assets/images/libro1.jpg",
@@ -81,50 +113,86 @@ class MyApp extends StatelessWidget {
                       ),
                     ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Column(
-                      children: <Widget>[
-                        Image.asset(
-                          "assets/images/libro2.jpg",
-                          width: 100,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset(
+                        "assets/images/libro2.jpg",
+                        width: 100,
+                      ),
+                      Text(
+                        "El nombre ",
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
                         ),
-                        Text(
-                          "El nombre ",
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                          ),
-                          textDirection: TextDirection.ltr,
-                        ),
-                      ],
-                    ),
+                        textDirection: TextDirection.ltr,
+                      ),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Column(
-                      children: <Widget>[
-                        Image.asset(
-                          "assets/images/libro3.jpg",
-                          width: 100,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset(
+                        "assets/images/libro3.jpg",
+                        width: 100,
+                      ),
+                      Text(
+                        "El nombre 2",
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
                         ),
-                        Text(
-                          "El nombre 2",
-                          style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                          ),
-                          textDirection: TextDirection.ltr,
+                        textDirection: TextDirection.ltr,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ]),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Top Authors",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.blue,
+                        height: 10,
+                        width: 10,
+                      )
+                    ],
+                  ),
+                  Container(
+                    height: 120,
+                    color: Colors.red,
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "assets/images/Authors/Patrick-rothfuss.jpg",
+                          height: 30,
                         ),
                       ],
                     ),
                   ),
                 ],
-              ),
-            ),
-          ])
+              )),
         ]),
       )),
     ));
