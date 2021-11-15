7cd4cd37eb74febf99e1aed8d4e8ad4335a14c6d
Francisco Cucaita <pacho.cucaita@gmail.com>
2021-11-15 15:39:31 -0300

    InputTextField

--- lib/app/ui/pages/register/register_page.dart
+++ lib/app/ui/pages/register/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:le_libros/widgets/input_textfiel.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -16,13 +17,49 @@ class RegisterPage extends StatelessWidget {
             padding: EdgeInsets.all(12),
             child: Column(
               children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(),
-                  ),
+                InputTextField(
+                  label: "Name",
                   onChanged: (text) {},
-                )
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                InputTextField(
+                  label: "Last Name",
+                  onChanged: (text) {},
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                InputTextField(
+                  label: "Email",
+                  inputType: TextInputType.emailAddress,
+                  onChanged: (text) {},
+                  validator: (text) {
+                    print('text $text');
+                    if (text == null) return null;
+                    if (text.contains("@")) {
+                      return null;
+                    }
+                    return "Invalid email";
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                InputTextField(
+                  label: "Password",
+                  onChanged: (text) {},
+                  isPassword: true,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                InputTextField(
+                  label: "Verify Password",
+                  onChanged: (text) {},
+                  isPassword: true,
+                ),
               ],
             ),
           ),
---
+++ lib/widgets/input_textfiel.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class InputTextField extends StatefulWidget {
+  final void Function(String)? onChanged;
+  final String label;
+  final TextInputType? inputType;
+  final bool isPassword;
+  final String? Function(String?)? validator;
+  const InputTextField(
+      {Key? key,
+      this.onChanged,
+      required this.label,
+      this.inputType,
+      this.isPassword = false,
+      this.validator})
+      : super(key: key);
+
+  @override
+  _InputTextFieldState createState() => _InputTextFieldState();
+}
+
+class _InputTextFieldState extends State<InputTextField> {
+  late bool _obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField(
+        validator: widget.validator,
+        autovalidateMode: AutovalidateMode.always,
+        builder: (state) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                obscureText: _obscureText,
+                keyboardType: widget.inputType,
+                onChanged: (text) {
+                  if (widget.validator != null) {
+                    // ignore: invalid_use_of_protected_member
+                    state.setValue(text);
+                    state.validate();
+                  }
+                  if (widget.onChanged != null) {
+                    widget.onChanged!(text);
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: widget.label,
+                  border: const OutlineInputBorder(),
+                  suffixIcon: widget.isPassword
+                      ? IconButton(
+                          onPressed: () {
+                            _obscureText = !_obscureText;
+                            setState(() {});
+                          },
+                          icon: Icon(_obscureText
+                              ? Icons.visibility
+                              : Icons.visibility_off))
+                      : Container(
+                          width: 0,
+                        ),
+                ),
+              ),
+              if (state.hasError)
+                Text(
+                  state.errorText!,
+                  style: TextStyle(color: Colors.redAccent),
+                )
+            ],
+          );
+        });
+  }
+}
+
+
+
+
+
+// class RegisterField extends StatelessWidget {
+//   final String text;
+
+//   const RegisterField({Key? key, required this.text}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextFormField(
+//       decoration: InputDecoration(
+//         labelText: text,
+//         border: const OutlineInputBorder(),
+//       ),
+//       onChanged: (text) {},
+//     );
+//   }
+// }
