133db54f7547c75f6ce7ef9013224d981bc63ce1
Francisco Cucaita <pacho.cucaita@gmail.com>
2021-11-21 22:25:21 -0300

    bloc Register

--- lib/app/ui/pages/register/bloc/register_bloc.dart
+++ lib/app/ui/pages/register/bloc/register_bloc.dart
@@ -1,20 +1,34 @@
 import 'package:bloc/bloc.dart';
 import 'package:le_libros/app/ui/pages/register/bloc/events/register_event.dart';
 import 'package:le_libros/app/ui/pages/register/bloc/states/register_state.dart';
+import 'package:le_libros/app/ui/routes/routes.dart';
+import 'package:le_libros/widgets/dialogs/progress_dialog.dart';
 
+import '../register_page.dart';
 import 'events/do_register_event.dart';
+import 'package:flutter_meedu/router.dart' as router;
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
   RegisterBloc(RegisterState initialState) : super(initialState) {
     on<DoRegisterEvent>((event, emit) async {
+      final controller = registerProvider.read;
+
       // event.name event.surname
+
       // pushea nueva pantalla con un loading
+      //ProgressDialog.show(context);
       // await registrarse
+      final response = await controller.submit();
       // pop del loading
+      router.pop();
       // si hay error entonces pushear un Dialog.alert(...)
-
+      if (response.error != null) {
+        print('Hay un error');
+      }
       // sino entonce pusheamos el home
-
+      else {
+        router.pushNamedAndRemoveUntil(Routes.Home);
+      }
       // sendRegisterForm(...)
       // no hay que hacer emit
     });
--- lib/app/ui/pages/register/register_page.dart
+++ lib/app/ui/pages/register/register_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_meedu/flutter_meedu.dart';
+import 'package:le_libros/app/ui/pages/register/bloc/events/do_register_event.dart';
 import 'package:le_libros/app/ui/pages/register/controller/register_controller.dart';
 import 'package:le_libros/app/ui/pages/register/controller/register_state.dart';
 import 'package:le_libros/app/ui/pages/register/controller/utils/send_register.form.dart';
@@ -9,19 +11,34 @@ import 'package:le_libros/widgets/input_textfiel.dart';
 
 import 'package:flutter_meedu/meedu.dart';
 
+import 'bloc/register_bloc.dart';
+
 final registerProvider = StateProvider<RegisterController, RegisterState>(
   (_) => RegisterController(),
 );
+//final bloc =RegisterBloc(..add(DoRegisterEvent()));
 
 class RegisterPage extends StatelessWidget {
   RegisterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ProviderListener<RegisterController>(
-      provider: registerProvider,
-      builder: (_, controller) {
-        return Scaffold(
+    //aca va el Bloc
+    // return BlocListener<RegisterBloc,RegisterState>(
+    //   bloc: bloc,
+    //   listener: (_,state));
+return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (state) {
+          return Get.i.find<RegisterBloc>()..add(DoRegisterEvent());
+        })
+      ],
+      child:   
+    // return ProviderListener<RegisterController>(
+    //   provider: registerProvider,
+    //   builder: (_, state) {
+    //     return 
+        Scaffold(
             appBar: AppBar(),
             body: Container(
                 child: GestureDetector(
@@ -31,13 +48,13 @@ class RegisterPage extends StatelessWidget {
                 width: double.infinity,
                 padding: EdgeInsets.all(8),
                 child: Form(
-                  key: controller.formKey,
+                  key: state.formKey,
                   child: SingleChildScrollView(
                     child: Column(
                       children: [
                         InputTextField(
                           label: "Name",
-                          onChanged: controller.OnNameChanged,
+                          onChanged: state.OnNameChanged,
                           validator: (text) {
                             if (text == null) return null;
                             return isValidName(text) ? null : "Invalid name ";
@@ -48,7 +65,7 @@ class RegisterPage extends StatelessWidget {
                         ),
                         InputTextField(
                           label: "Last Name",
-                          onChanged: controller.OnLastNameChanged,
+                          onChanged: state.OnLastNameChanged,
                           validator: (text) {
                             if (text == null) return null;
                             return isValidName(text)
@@ -62,7 +79,7 @@ class RegisterPage extends StatelessWidget {
                         InputTextField(
                           label: "Email",
                           inputType: TextInputType.emailAddress,
-                          onChanged: controller.OnEmailChanged,
+                          onChanged: state.OnEmailChanged,
                           validator: (text) {
                             if (text == null) return null;
                             return isValidEmail(text) ? null : "Invalid email";
@@ -73,7 +90,7 @@ class RegisterPage extends StatelessWidget {
                         ),
                         InputTextField(
                           label: "Password",
-                          onChanged: controller.OnPasswordChanged,
+                          onChanged: state.OnPasswordChanged,
                           isPassword: true,
                           validator: (text) {
                             if (text == null) return null;
@@ -91,11 +108,11 @@ class RegisterPage extends StatelessWidget {
                           //watch(registerProvider.select((state) => state.password),);
                           return InputTextField(
                               label: "Verify Password",
-                              onChanged: controller.OnVPasswordChanged,
+                              onChanged: state.OnVPasswordChanged,
                               isPassword: true,
                               validator: (text) {
                                 if (text == null) return null;
-                                if (controller.state.password != text) {
+                                if (state.state.password != text) {
                                   return "la contraseña no conicide";
                                 }
                                 if (text.trim().length >= 6) {
